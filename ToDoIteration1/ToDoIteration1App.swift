d7514c4fce64268cb830ce85873928abcb88c279
Tracey Bullington <traceylbullington@gmail.com>
2023-05-20 12:04:37 -0500

    Initial Commit

---
+++ ToDoIteration1/ToDoIteration1App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoIteration1App.swift
+//  ToDoIteration1
+//
+//  Created by scholar on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoIteration1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
