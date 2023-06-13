76ea80e51cb10ccff48c2a885143920120dbf37c
Tracey Bullington <traceylbullington@gmail.com>
2023-06-13 15:40:39 -0500

    initial commit of iteration1

--- ToDoIteration1/ContentView.swift
+++ ToDoIteration1/ContentView.swift
@@ -1,26 +1,41 @@
-//
-//  ContentView.swift
-//  ToDoIteration1
-//
-//  Created by scholar on 5/20/23.
-//
-
 import SwiftUI
 
+
 struct ContentView: View {
+    @State var toDoItems: [ToDoItem] = []
+    @State private var showNewTask = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("To Do List")
+                    .font(.system(size: 40))
+                    . fontWeight(.black)
+                Spacer()
+                Button(action: {
+                    self.showNewTask = true
+                }){
+                    Text("+")
+                }
+            }
+            .padding()
+            Spacer()
+            List {
+                ForEach (toDoItems) { toDoItem in if toDoItem.isImportant == true {
+                     Text("!!" + toDoItem.title)
+                } else {
+                    Text(toDoItem.title)
+                }
+                }
+            }
         }
-        .padding()
+        if showNewTask {
+            NewToDoView(toDoItems: $toDoItems, showNewTask: $showNewTask, title: "", isImportant: false)
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
